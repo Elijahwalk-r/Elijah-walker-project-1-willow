a8882cfc8c0ebde39ef97dbeacc10495bc6132c7
ElijahW <elijahwalk.r@gmail.com>
2022-10-29 13:25:13 -0400

    styles

--- scripts.js
+++ scripts.js
@@ -1,15 +1,3 @@
-// checking to see if my JavaScript is working through a simple console.log
-// let eli = (1 + 1)
-// console.log(eli);
-
-
-// PSEUDO CODE
-// Grab my form elements (buttons) and prevent the page from refreshing when a user selects it
-//     1. Create a variable to hold my form element through the Document object model utilizing a query selector
-//     2. Add an event listener that listens to the node once it is clicked
-//     3. Add a preventDefault function to prevent the page form refreshing
-//     4. Trigger an alert function to let the user know the submit button is inactive 
-
 
 //1.
 const willowForm = document.querySelector('form');
